15c38d19726b5223d3c98f77488a370bf6ea9b14
egoing <egoing@gmail.com>
2013-07-07 14:00:32 +0900

    * 발송한 이메일의 숫자 표시  * 소식 구독을 취소 할 수 있는 구독 취소 기능 구현  * 로그인 대상을 관리자로 변경

--- controller.py
+++ controller.py
@@ -47,6 +47,7 @@ class Member(ndb.Model):
   notified = ndb.DateTimeProperty(auto_now_add=True)
   twitter = ndb.StringProperty(required=False)
   phone = ndb.StringProperty(required=False)
+  token = ndb.StringProperty(required=True)
 
 
 JINJA_ENVIRONMENT = jinja2.Environment(
@@ -56,12 +57,23 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 class CommonRequest(webapp2.RequestHandler):
   global _config, theme
   bttheme = theme[0]  
+  def require_admin(self):
+    user = users.get_current_user()
+    if user:
+      if users.is_current_user_admin():
+        return True
+      else:
+        self.redirect('/error?id=1')
+    else:
+      self.redirect(users.create_login_url(self.request.uri))
+
   def is_admin(self):
     user = users.get_current_user()    
-    if user and user.email() == _config.ADMIN_ID: 
+    if user and users.is_current_user_admin():
       return True
     else:
       return False
+
   def get_login_url(self):
     return users.create_login_url(self.request.uri)
 
@@ -70,19 +82,25 @@ class MainPage(CommonRequest):
     template = JINJA_ENVIRONMENT.get_template('register.html')
     self.response.out.write(template.render({'is_admin':super(MainPage, self).is_admin(), 'theme': CommonRequest.bttheme, 'login_url': self.get_login_url()}))
 
+class Error(CommonRequest):
+  def get(self):
+    template = JINJA_ENVIRONMENT.get_template('error.html')
+    id = str(self.request.get('id'))
+    if id == '1':
+      message = '관리자만 접근 할 수 있는 페이지 입니다'
+    self.response.out.write(template.render({'message':message.decode("utf8"), 'theme': CommonRequest.bttheme}))
+
 class List(CommonRequest):
   def get(self):    
-    if not super(List, self).is_admin():
-       self.redirect(users.create_login_url(self.request.uri))
+    super(List, self).require_admin()    
     members = Member.query().order(-Member.created).fetch(1000)
     template = JINJA_ENVIRONMENT.get_template('list.html')
     self.response.out.write(template.render({'members':members, 'is_admin':super(List, self).is_admin(), 'theme': CommonRequest.bttheme, 'login_url': self.get_login_url()}))
 
 class Process(CommonRequest):  
   def post(self):
-    import json, sys    
+    import json, sys, random, string    
     from google.appengine.api import mail
-    logging.info(mail.is_email_valid(self.request.get('email')))
     if not mail.is_email_valid(self.request.get('email')):
       self.response.out.write(json.dumps({'result':False, 'data':None, 'msg':'이메일 주소를 올바르게 입력해주세요', 'errorCode':1}))    
       return
@@ -92,31 +110,22 @@ class Process(CommonRequest):
     member.region = self.request.get('region')
     member.comment = self.request.get('comment')
     member.twitter = self.request.get('twitter')
-    member.phone = self.request.get('phone')    
+    member.phone = self.request.get('phone')
+    member.token = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(15))
     member.put()
     self.response.out.write(json.dumps({'result':True, 'data':None, 'msg':None, 'errorCode':None}))    
-
-class Guestbook(CommonRequest):
-  def post(self):
-    greeting = Greeting(parent=guestbook_key)    
-
-    if users.get_current_user():
-      greeting.author = users.get_current_user()
-
-    greeting.content = self.request.get('content')
-    greeting.put()
-    self.redirect('/')
 
 class Send_process(CommonRequest):  
   global _config
   def post(self):    
     if not super(Send_process, self).is_admin():
        self.redirect(users.create_login_url(self.request.uri))
-    import json
+    import json, urllib
     from google.appengine.api import mail
     from google.appengine.ext import deferred
     title = self.request.get('title')
     message = self.request.get('message')
+    send_number = 0
     if title and message:
       members = Member.query().fetch()
       sender_list = []
@@ -124,13 +133,28 @@ class Send_process(CommonRequest):
         if not mail.is_email_valid(member.email):
           continue
         else:          
-          #mail.send_mail(sender = _config.SENDER_EMAIL_ADDRESS, to = member.email, subject = title, body = message)
-          deferred.defer(mail.send_mail, sender = _config.SENDER_EMAIL_ADDRESS, to = member.email, subject = title, body = message)
+          try:
+            query = urllib.urlencode({'email':member.email, 'token':member.token})
+          except UnicodeEncodeError:
+            logging.info('UnicodeEncodeError : ' + member.email)            
+            continue
+          unsubscribe_link = u'<a href="https://%s/unsubscribe?%s">구독취소</a>' % (self.request.host, query)
+          deferred.defer(mail.send_mail, sender = _config.SENDER_EMAIL_ADDRESS, to = member.email, subject = title, body = message, html = message+'<br /><br />'+unsubscribe_link)
           member.notified = datetime.datetime.now()
-          member.put()      
-      self.response.out.write(json.dumps({'result':True, 'data':None, 'msg':None, 'errorCode':None}))
-    else:      
+          member.put()
+          send_number += 1
+      self.response.out.write(json.dumps({'result':True, 'data':send_number, 'msg':None, 'errorCode':None}))
+    else:
       self.response.out.write(json.dumps({'result':False, 'data':None, 'msg':'parameter quantity is wrong', 'errorCode':1}))
+
+class Unsubscribe(CommonRequest):
+  def get(self):
+    member = Member.query(Member.email == self.request.get('email'), Member.token == self.request.get('token')).get()
+    if(member):
+      member.key.delete()
+      self.response.out.write('<html><body><script>alert("구독을 취소 했습니다.");history.back()</script></body></html>')      
+    else:
+      self.response.out.write('<html><body><script>alert("존재하지 않는 계정입니다.");history.back()</script></body></html>')      
 
 class Delete_process(CommonRequest):
   def post(self):
@@ -147,9 +171,8 @@ class Delete_process(CommonRequest):
 
 class Migration(CommonRequest):
   def get(self):
-    if not super(Migration, self).is_admin():
-       self.redirect(users.create_login_url(self.request.uri))
-    import csv
+    super(Migration, self).require_admin()  
+    import csv, random, string
     with open('jacsim.csv', 'rb') as csvfile:
       c = csv.reader(csvfile)
       i = 0
@@ -162,29 +185,30 @@ class Migration(CommonRequest):
         member = Member(parent = member_key)
         member.nickname = nickname
         member.email = email
-        member.comment = comment
+        member.comment = comment        
+        member.token = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(15))
         i = i + 1
-        member.put()
+        future = member.put_async()
+        future.get_result()
     self.response.out.write(i)
 
 class Reset(CommonRequest):
   def get(self):
-    if not super(Reset, self).is_admin():
-       self.redirect(users.create_login_url(self.request.uri))
+    super(Reset, self).require_admin()  
     members = Member.query().fetch()
     for m in members:
-      logging.info(m)
       logging.info(m.key.delete())
 
 app = webapp2.WSGIApplication([
   ('/', MainPage),
-  ('/sign', Guestbook),
   ('/process', Process),
   ('/list', List),
   ('/send_process', Send_process),
   ('/migration', Migration),
   ('/reset', Reset),
-  ('/delete_process', Delete_process)
+  ('/error', Error),
+  ('/delete_process', Delete_process),
+  ('/unsubscribe', Unsubscribe)
 ], debug=True)
 
 def main():
